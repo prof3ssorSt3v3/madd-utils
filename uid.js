24ae4981c6c189cc52869cc6c9aedacc7ae63518
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2022-01-06 14:42:16 -0500

    update uid to use base 16 for time

--- uid.js
+++ uid.js
@@ -1,10 +1,14 @@
 //create a random user id
 //can pass in an optional Boolean to add a hyphen and timestamp
-//all values are in base 36
+//base value in base 36
+//time value in base 16
 const uid = (withTime) => {
   let id = Math.random().toString(36).substring(2, 16).padEnd(16, '0');
   if (withTime) {
-    id += `-${Date.now().toString(36)}`;
+    id += `-${Date.now().toString(16)}`;
   }
   return id;
 };
+
+// console.log(uid());
+// console.log(uid(true));
